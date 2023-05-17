663bf08e761cb14bef7861c200058cc4c36500c5
Bruno Schüty <120675337+schutyb@users.noreply.github.com>
2023-05-16 23:36:11 -0300

    seleccione las regiones de interes de la 18852

--- rois_tile.py
+++ rois_tile.py
@@ -11,20 +11,28 @@ dc, g, s, md, ph = hsitools.tilephasor(im, dimx=1024, dimy=1024)
 # dc, g, s, md, ph = hsitools.phasor(im)
 n = 6
 m = 2
-b = False
 
-dc = hsitools.tile_stitching(dc, n, m, bidirectional=b)
-g = hsitools.tile_stitching(g, n, m, bidirectional=b)
-s = hsitools.tile_stitching(s, n, m, bidirectional=b)
-# ph = hsitools.tile_stitching(ph, n, m, bidirectional=b)
-# md = hsitools.tile_stitching(md, n, m, bidirectional=b)
+concat = True
+if concat:
+    b = False
+    dc = hsitools.tile_stitching(dc, n, m, bidirectional=b)
+    g = hsitools.tile_stitching(g, n, m, bidirectional=b)
+    s = hsitools.tile_stitching(s, n, m, bidirectional=b)
+    # ph = hsitools.tile_stitching(ph, n, m, bidirectional=b)
+    # md = hsitools.tile_stitching(md, n, m, bidirectional=b)
 
-# phint = np.array([90, 160])
-# mdint = np.array([0.4, 0.8])
-# hsi_visualization.interactive2(dc, g, s, 8, ph, phint, modulation=md, mdint=mdint,
-# histeq=False, filt=True, nfilt=3)
+ver = True
+if ver:
+    # phint = np.array([90, 160])
+    # mdint = np.array([0.4, 0.8])
+    # hsi_visualization.interactive2(dc, g, s, 8, ph, phint, modulation=md, mdint=mdint,
+    # histeq=False, filt=True, nfilt=3)
+    #
+    # dc2 = dc[1600:4400, 430:1030]
+    # g2 = g[1600:4400, 430:1030]
+    # s2 = s[1600:4400, 430:1030]
 
-hsi_visualization.interactive1(dc, g, s, 0.1, nbit=8,
-                               ncomp=5, histeq=True, filt=True, nfilt=3,
-                               spectrums=False, hsi_stack=np.asarray(im),
-                               lamd=np.arange(415, 715, 10))
+    hsi_visualization.interactive1(dc, g, s, 0.1, nbit=8,
+                                   ncomp=5, histeq=True, filt=True, nfilt=3,
+                                   spectrums=False, hsi_stack=np.asarray(im),
+                                   lamd=np.arange(415, 715, 10))
