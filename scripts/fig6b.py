489e74f10906e3276ba3177511b680e8f53ff1e7
Bruno Schüty <120675337+schutyb@users.noreply.github.com>
2023-05-17 19:42:11 -0300

    Add files via upload

---
+++ scripts/fig6b.py
@@ -0,0 +1,43 @@
+import numpy as np
+from hsipy import hsitools, hsi_visualization
+import tifffile
+import matplotlib.pyplot as plt
+from skimage.filters import median
+import phasorlibrary as phlib
+from matplotlib import colors
+import os
+from sklearn.cluster import KMeans
+import pandas as pd
+
+
+cal = False
+if cal:
+    names = os.listdir('/home/bruno/Documentos/Proyectos/hsimel/phasors/nevo/')
+    imd = np.zeros(len(names))
+    iph = np.zeros(len(names))
+
+    for i in range(len(names)):
+        im = tifffile.imread('/home/bruno/Documentos/Proyectos/hsimel/phasors/nevo/' + names[i])
+
+        binsph = np.arange(45, 180)
+        binsmd = np.linspace(0, 1, 101)
+        histmd = np.histogram(im[4].flatten(), bins=binsmd)[0][1:]
+        histph = np.histogram(im[3].flatten(), bins=binsph)[0][1:]
+
+        imd[i] = phlib.center_of_mass(binsmd[:99], histmd)
+        iph[i] = phlib.center_of_mass(binsph[:133], histph)
+
+        df = pd.DataFrame(list(zip(*[imd, iph]))).add_prefix('Col')
+        df.to_csv('/home/bruno/Documentos/Proyectos/hsimel/cmn.csv', index=False)
+
+plotty = True
+if plotty:
+    datam = pd.read_csv('/home/bruno/Documentos/Proyectos/hsimel/cmm.csv')
+    datan = pd.read_csv('/home/bruno/Documentos/Proyectos/hsimel/cmn.csv')
+
+    plt.figure(1)
+    plt.plot(datam["Col0"], datam["Col1"], 'ro', label='melanoma')
+    plt.plot(datan["Col0"], datan["Col1"], 'bo', label='nevo')
+    plt.legend()
+    plt.grid()
+    plt.show()
